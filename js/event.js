9cba48f511e26f030823039a74237ea15472f63a
developerTamanna <astamanna57@gmail.com>
2025-02-23 10:53:41 +0600

    note and addevenlistener

--- js/event.js
+++ js/event.js
@@ -24,3 +24,26 @@ btnMakePurple.onclick = makePurple;
 function makePurple(){
    document.body.style.backgroundColor = 'purple';
 }
+
+
+//< --option 4: addEventlistener-->
+
+//   *  document.getElementById()addEventListener('evenType', handler function)
+// 1: getElementById
+// 2: getElementById.addEventListener
+document.getElementById('btn-make-green').addEventListener('click', function makeGreen(){
+    // console.log(4444)
+    document.body.style.backgroundColor ='green'
+});
+// next
+document.getElementById('btn-make-gold')
+.addEventListener('click', function() {
+    document.body.style.backgroundColor = 'gold'; 
+});
+
+
+// next
+const btn = document.getElementById('btn');
+btn.addEventListener('click', function(){
+    alert('button clicked');
+});
